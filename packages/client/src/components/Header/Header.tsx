654d8e6c573d48c02fc638bd8b418da822168bec
Ilmar <ilmar.ilves44@gmail.com>
2022-01-18 20:27:11 +0200

    Added initial header component

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -4,7 +4,16 @@ import "./header.scss";
 export const Header: React.FC = () => {
   return (
     <>
-      <div className={"header"}>P</div>
+      <div className={"header"}>
+        <div className={"logo-container"}>
+          <span>O</span>
+          <h1>Muugal</h1>
+        </div>
+        <div className={"login-container"}>
+          <span>Log in</span>
+          <span>Order</span>
+        </div>
+      </div>
     </>
   );
 };
