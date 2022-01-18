afc0c94b9f2ad97cb8c4b263e733b34f3ed19569
Ilmar <ilmar.ilves44@gmail.com>
2022-01-18 18:50:54 +0200

    Added react router dependency

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import { LandingPageView } from "./views/LandingPageView/LandingPageView";
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <p>muugal</p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPageView />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
-
-export default App;
+};
---
+++ packages/client/src/views/LandingPageView/LandingPageView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LandingPageView: React.FC = () => {
+  return (
+    <>
+      <p>muugal</p>
+    </>
+  );
+};
