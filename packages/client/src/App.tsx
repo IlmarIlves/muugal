fe6ee0dd2be13b17a8b8d0794c41b531a3d7c198
Ilmar <ilmar.ilves44@gmail.com>
2022-01-25 20:35:58 +0200

    Added logged in user routing draft

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
-import { Route, Routes } from "react-router";
+import { Route, RouteProps, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import { LandingPageView } from "./views/LandingPageView/LandingPageView";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import { LoginView } from "./views/LoginView/LoginView";
+import { NotFoundView } from "./views/NotFoundView/NotFoundView";
+
+export interface ProtectedRouteProps extends RouteProps {
+  isAuthenticated: boolean;
+  authenticationPath: string;
+}
 
 export const App: React.FC = () => {
   return (
@@ -10,6 +17,9 @@ export const App: React.FC = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPageView />} />
+          <Route path="*" element={<NotFoundView />} />
+          {/* <Route path="dashboard" element={<PrivateRoute roles={[ROLE.ADMIN]} component={Dashboard} />} />
+          <Route path="users" element={<PrivateRoute roles={[ROLE.ADMIN, ROLE.USER]} component={Users} />} /> */}
         </Routes>
       </BrowserRouter>
     </>
